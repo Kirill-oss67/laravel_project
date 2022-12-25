f44d79e19265b74b2926d512b18909338d443298
Kirill-oss67 <yakovkirill-roslavl@yandex.ru>
2022-12-25 18:14:41 +0300

    start project

---
+++ app/Http/Controllers/MyPlaceController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyPlaceController extends Controller
+{
+    public function index(){
+        return 'this is my page';
+    }
+}
