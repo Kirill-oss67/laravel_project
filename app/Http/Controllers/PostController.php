7c735a64203d44be4b407be6f4d2a3766f741a33
Kirill-oss67 <yakovkirill-roslavl@yandex.ru>
2022-12-30 18:46:47 +0300

    introduce with view and blade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,14 +10,15 @@ class PostController extends Controller
     public function index()
     {
         // $post = Post::find(1);
-        $posts = Post::all();
+        $posts = Post::all(); // поиск всех постов
         // $posts = Post::where('is_published', 0)->first(); // поиск по критериям
         // foreach($posts as $post){
         //     dump($post->title);
         // }
 
         // dd('end');
-        dd($posts);
+        // dd($posts);
+        return view('posts', compact('posts'));
     }
     public function create()
     {
